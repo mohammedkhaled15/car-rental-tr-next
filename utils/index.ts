4627ab2b1aa01e03341d63bee8f963a08f6f9115
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-08-07 19:42:05 +0300

    ..

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
 import { CarProps } from "@/types";
+import { FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { model, manufacturer, year, fuel, limit } = filters;
   const headers = {
     "X-RapidAPI-Key": "159f07ac27msh71daf479e1b2630p147eb3jsnabcc64a2ff6d",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&model=${model}&year=${year}&limit=${limit}&fuel=${fuel}`,
     { headers }
   );
   const result = await response.json();
