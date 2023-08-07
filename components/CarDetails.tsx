064cf15e64bc6d54714c338b82be457f8cf7392f
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-08-07 14:32:58 +0300

    ..

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -48,7 +48,45 @@ const CarDetails = ({ isOpen, car, closeModal }: CarDetailsProps) => {
                   </button>
 
                   <div className="flex-1 flex flex-col gap-3">
-                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg"></div>
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src={"/hero.png"}
+                        alt="car model"
+                        fill
+                        className="object-contain"
+                      />
+                    </div>
+
+                    <div className="flex gap3">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car model"
+                          fill
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car model"
+                          fill
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car model"
+                          fill
+                          className="object-contain"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2></h2>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
